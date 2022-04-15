9f1a6bcdd778f3b31d655d2494e5e8c28d1e4005
Zsolt Tóth <zsolttothofficial@gmail.com>
2022-04-15 18:48:30 +0200

    Initial Commit

---
+++ miniDating/miniDatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniDatingApp.swift
+//  miniDating
+//
+//  Created by Zsolt Toth on 2022. 04. 15..
+//
+
+import SwiftUI
+
+@main
+struct miniDatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
